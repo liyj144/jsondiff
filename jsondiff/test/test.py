dce2af96542cb986dd1bd927972faf8c505364d9
liyj144 <liyj144@163.com>
2017-05-08 15:41:22 +0800

    添加selenium测试

--- jsondiff/test/test.py
+++ jsondiff/test/test.py
@@ -3,6 +3,7 @@
 import json_diff
 from StringIO import StringIO
 import json
+from selenium import webdriver
 
 if __name__ == '__main__':
     str1 = '{"fixedDataStruct":"wandacredit","resultCode":"10000","resultMessage":"处理失败","dataMap":{"errorMessage":"session过期,请使用refreshToken更新","errorDisplay":8,"errorCode":"5001"}}'
@@ -10,3 +11,9 @@ if __name__ == '__main__':
     # print json.loads(str1)
     diffator = json_diff.Comparator(StringIO(str1), StringIO(str2))
     print diffator
+
+    options = webdriver.ChromeOptions()
+    options.add_argument(
+        '--user-agent=Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36')
+    driver = webdriver.Chrome(chrome_options=options)
+    driver.get('http://www.baidu.com')
